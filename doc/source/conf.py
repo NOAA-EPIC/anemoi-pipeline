6034bed237ba817bf16ce7fa1e0f7c1e2aa84fef
gspetro-NOAA <gillian.petro@noaa.gov>
2024-11-27 11:26:13 -0500

    update formatting

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,3 +42,10 @@ html_theme_options = {
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Add any paths that contain custom static files (such as style sheets) here.
+# They are copied after the built-in static files, so a file named "default.css" 
+# will overwrite the built-in "default.css".
+
+def setup(app):
+    app.add_css_file('theme_overrides.css')
