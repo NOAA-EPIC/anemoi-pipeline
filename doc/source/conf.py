6d6840bd7b3b1a6c818865c995fa42b087b26ee8
gspetro-NOAA <gillian.petro@noaa.gov>
2024-11-27 11:14:12 -0500

    update conf.py settings w/extensions

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,15 +14,31 @@ release = 'develop'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.extlinks',
+    #'sphinxcontrib.bibtex',
+]
+
+# bibtex_bibfiles = ['references.bib']
+
+numfig = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+html_theme_options = {
+    "body_max_width": "none", 
+    "navigation_depth": 4,
+    }
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
